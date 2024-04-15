2da489b472e182abb1e3756ff8328475b2001f9b
Kien <kien@notional.ventures>
2024-04-15 08:41:40 +0700

    Kien/e2e test migrate (#23)
    * feat: run locanode and push wasm code
    
    * feat: setup relayer
    
    * feat: using ngit for repo with nix
    
    * chore: update notes install deps
    
    * feat: script to upgrade
    
    * fix: upgrade scripts
    
    * fix: init deps script
    
    * fix/migration: wrong prefix relay addresS

--- scripts/upgrade/init_deps.sh
+++ scripts/upgrade/init_deps.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
 
-OLD_VERSION=v6.5.2
+OLD_VERSION=kien-devnet-651
 SOFTWARE_UPGRADE_NAME="v6_6_0"
 ROOT=$(pwd)
 
-COMPOSABLE_VERSION="pfm-fix"
+COMPOSABLE_VERSION="branchfortestingpfmfix"
 
-if [ ! -f "_build/composable/$COMPOSABLE_VERSION.zip" ] &> /dev/null
-then
-    mkdir -p _build/composable
-    wget -c "https://github.com/notional-labs/composable/archive/refs/tags/${COMPOSABLE_VERSION}.zip" -O _build/composable/${COMPOSABLE_VERSION}.zip
-    unzip _build/composable/${COMPOSABLE_VERSION}.zip -d _build/composable
+mkdir -p _build/
+
+
+# Check if the directory exists and is a Git repository
+# TODO: using git, since nix in composable repo requires something with git
+# Consider using submodule, or firgure this out
+if [ ! -d "_build/composable/.git" ]; then
+    cd _build/
+    git clone https://github.com/ComposableFi/composable.git composable
+    cd composable
+    git checkout "$COMPOSABLE_VERSION"
+    cd ../.././
 fi
-
 
 
 # install old binary if not exist
 if [ ! -f "_build/$OLD_VERSION.zip" ] &> /dev/null
 then
     mkdir -p _build/old
-    wget -c "https://github.com/ComposableFi/composable-cosmos/archive/refs/tags/${OLD_VERSION}.zip" -O _build/${OLD_VERSION}.zip
+    wget -c "https://github.com/notional-labs/composable-cosmos/archive/refs/tags/${OLD_VERSION}.zip" -O _build/${OLD_VERSION}.zip
     unzip _build/${OLD_VERSION}.zip -d _build
 fi
 
 # reinstall old binary
 if [ $# -eq 1 ] && [ $1 == "--reinstall-old" ] || ! command -v _build/old/centaurid &> /dev/null; then
-    cd ./_build/composable-cosmos-${OLD_VERSION:1}
+    cd ./_build/composable-cosmos-${OLD_VERSION}
     GOBIN="$ROOT/_build/old" go install -mod=readonly ./...
     cd ../..
 fi
